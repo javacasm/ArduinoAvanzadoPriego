733b5e3642e04f1fd00ca55a5cb65b3b5dd19285
javacasm <javacasm@gmail.com>
2018-02-15 12:19:03 +0100

    codigo

---
+++ codigo/Blink_pot/Blink_pot.ino
@@ -0,0 +1,44 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  int valorHumedad = analogRead(A0);     // entre 0 y 1023
+  Serial.print("Humedad=");
+  Serial.println(valorHumedad);
+  delay(200);   // 299 milisegundos
+  /*
+  int espera = map(valorPot,0,1023,50,2000);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(espera);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(espera);                       // wait for a second
+  */
+}
