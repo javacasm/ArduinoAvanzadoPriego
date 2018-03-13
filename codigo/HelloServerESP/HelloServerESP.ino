de77a7aea128a5ddd686fb27c88fe5d8431923af
javacasm <javacasm@gmail.com>
2018-03-13 11:35:04 +0100

    add

---
+++ codigo/HelloServerESP/HelloServerESP.ino
@@ -0,0 +1,79 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+//const char* ssid = "CEP_MONTILLA-UNIFI";
+const char* ssid = "TP-LINK_A93F0F";
+//const char* password = "CEPMONTILLA";
+const char* password = "C1A93F0F";
+
+ESP8266WebServer server(80);
+
+const int led = D0;
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  String CodigoPagina = "<html><body>";
+  CodigoPagina += "<p>Temperatura = " + temp;
+  CodigoPagina += "<p>humedad = " + hum;
+  CodigoPagina += "</body></html>";
+  server.send(200, "text/plain", CodigoPagina);
+  digitalWrite(led, 0);
+}
+
+void handleNotFound(){
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void setup(void){
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+}
